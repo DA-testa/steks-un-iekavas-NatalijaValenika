8cbde718f2db23236366795cfa10fc436fd0cd1e
NatalijaValenika <125027023+NatalijaValenika@users.noreply.github.com>
2023-02-20 00:51:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,39 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i+1))
             pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
+            if not opening_brackets_stack:
+                return i+1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i+1
             pass
-
-
+        
+        if opening_brackets_stack:
+            return opening_brackets_stack[0].position
+        returns "Success"
+            
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    text = input("F or I")
+    if "F" in text:
+        name input("Enter gile name: ")
+        with open(name, "r", enconding="latinl") as file:
+            text1=file.read()
+        mismatch = find.mismatch(text1)
+        if mismatch == "Success":
+            print("Success")
+        else:
+            print(mismatch)
+        eleif "I" in text:
+            text1 = input()
+            mismatch = find_mismatch(text1)
+            if mismatch == "Success":
+                print("Success")
+            else:
+                print (mismatch)
 
 
 if __name__ == "__main__":
