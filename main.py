eeadcfe0be460f4d49a2f886cf7126c986184ad1
NatalijaValenika <125027023+NatalijaValenika@users.noreply.github.com>
2023-02-20 01:36:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
+        if next == "\n":
+            continue
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i+1))
 
